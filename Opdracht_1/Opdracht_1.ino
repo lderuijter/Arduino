e31643c13326fc83cc19aef15e86d3674b4c64f7
lderuijter <85785995+lderuijter@users.noreply.github.com>
2026-06-23 12:03:55 +0200

    add comments

--- Opdracht_1/Opdracht_1.ino
+++ Opdracht_1/Opdracht_1.ino
@@ -1,16 +1,19 @@
 void setup() {
-  // put your setup code here, to run once:
+  // output geven aan de pins die gebruikt worden
   pinMode(11, OUTPUT);
   pinMode(13, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // led 11 uit, 13 aan
   digitalWrite(11, LOW);
   digitalWrite(13, HIGH);
+  // 2 seconden wachten
   delay(2000);
-  
+
+  // led 13 uit, 11 aan
   digitalWrite(13, LOW);
   digitalWrite(11, HIGH);
+  // 2 seconden wachten
   delay(2000);
 }
